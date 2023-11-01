ae6774a0c89d381d446effb2115221fb6712f753
katyasolis <149627423+katyasolis@users.noreply.github.com>
2023-11-01 16:15:24 -0500

    Servo Example Code

---
+++ servo_stuff.ino
@@ -0,0 +1,18 @@
+#include <Servo.h> 
+Servo myservo;
+const int buttonPin = 2; 
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(buttonPin) == HIGH){
+    myservo.write(180);
+  } else { 
+    myservo.write(0);
+  }
+}
