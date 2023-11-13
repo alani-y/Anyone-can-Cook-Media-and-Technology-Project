d442b876454cca981fa836376167e252a42ca39e
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-13 15:19:19 -0600

    Update servo_stuff.ino
    made it so the servo will rotate when a button is pressed

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -3,18 +3,32 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
+const int buttonPin = 2;
 int pos = 0;    // variable to store the servo position
-float inputVoltage = 0.0; //variable to store the voltage reading
+int buttonState = 0; // button status variable
+int inputVoltage = 0;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600); 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo 
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    Serial.println("Hi");
+    rotateServo();
+  }
+
+}
+
+int rotateServo(){
   int analogValue = analogRead(A5);
   inputVoltage = (analogValue * 5.0)/ 1024.0;
-  roundedVoltage = round(inputVoltage);
-  Serial.println(roundedVoltage);
+  Serial.println(inputVoltage);
+  Serial.print("yes");
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
