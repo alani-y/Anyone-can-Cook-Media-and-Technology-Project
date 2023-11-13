1a3ba4c70729bbba320b40589318b1ec5ffa6e54
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-13 16:42:17 -0600

    Update servo_stuff.ino
    simplified the function that moves the servo

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -19,8 +19,7 @@ void loop() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
     Serial.println("HIGH");
-    Serial.println("Hi");
-    rotateServo();
+    rotateServo(180);
   }
   else{
     Serial.println("low");
@@ -28,34 +27,20 @@ void loop() {
 
 }
 
-int rotateServo(){
+int rotateServo(int x){
   //int analogValue = analogRead(A5);
   //inputVoltage = (analogValue * 5.0)/ 1024.0;
   //Serial.println(inputVoltage);
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= x; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = x; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
 
-int hundredServo(){
-  //int analogValue = analogRead(A5);
-  //inputVoltage = (analogValue * 5.0)/ 1024.0;
-  //Serial.println(inputVoltage);
-  Serial.println("yes");
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
+
 
