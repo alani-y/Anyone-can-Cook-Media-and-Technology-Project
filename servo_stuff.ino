eb6e27bec36e4f51437c5921e2a905371fa61f91
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-13 22:44:14 -0600

    Update servo_stuff.ino
    I changed the rotateServo function so it could be used for all of our servos instead of needing to code individual functions for each

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -1,54 +1,57 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo floorservo1;
+Servo floorservo2;
+Servo fridgeservo;
+Servo armservo;
+Servo remyservo;
+
+Servo servo[5] = {floorservo1, floorservo2, fridgeservo, armservo, remyservo};
 
 const int buttonPin = 2;
-const int fridgePin = 4;
+const int fridgePin = 7;
+const int servoPins[] = {9};
 int pos = 0;    // variable to store the servo position
-int inputVoltage = 0;
-
 int buttonState = 0; // button status variable
+int fridgeState = 0; // if the fridge is open or not
 
 void setup() {
   Serial.begin(9600); 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo 
+  for(int i = 0; i < 1; i++)
+  {
+    servo[i].attach(servoPins[i]);
+  }
   pinMode(buttonPin, INPUT);
   pinMode(fridgePin, INPUT);
 }
 
 void loop() {
+  Serial.println(servoPins[0]);
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
     Serial.println("HIGH");
-    rotateServo(180);
+    rotateServo(180, 0);
   }
   else{
     Serial.println("low");
   }
-  
-  Serial.println(digitalRead(fridgePin));
-
-  if (digitalRead(fridgePin) == 0) {  // this function opens the fridge when linguine approaches
-    rotateServo(90);
-  }
-
-}
-
-int rotateServo(int x){
-  //int analogValue = analogRead(A5);
-  //inputVoltage = (analogValue * 5.0)/ 1024.0;
-  //Serial.println(inputVoltage);
-  for (pos = 0; pos <= x; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = x; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  fridgeState = digitalRead(fridgePin);
+  Serial.println(fridgeState);
+  if (fridgeState == 1) {
+    rotateServo(90, 2);
   }
 }
 
 
+int rotateServo(int degrees, int i){
 
+  for (pos = 0; pos <= degrees; pos += 1) { // goes from 0 degrees to 180 degrees
+    Serial.println("hi");
+    servo[i].write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = degrees; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo[i].write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
