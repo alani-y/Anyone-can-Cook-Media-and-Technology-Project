13d4bf60c463078f1012b2c5d883f93e74ea2068
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-15 15:37:37 -0600

    Update servo_stuff.ino

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -5,16 +5,16 @@ Servo floorservo2;
 Servo fridgeservo;
 Servo armservo;
 
-Servo servo[] = {fridgeservo, floorservo, floorservo2, armservo}; // This array stores all the servo objects
+Servo servo[] = {fridgeservo, armservo}; // This array stores all the servo objects
 
 const int buttonPin = 12;
 const int buttonPinMove = 4;
-const int fridgePin = 11;
-const int servoPins[] = {3, 5, 9, 10};
+const int linguinePositionPin = 8;
+const int servoPins[] = {3, 5};
 int pos = 0;    // variable to store the servo position
 int buttonState = 0; // button status variable
-int fridgeState = 0; // if the fridge is open or not
 bool rotateOnce = true;
+int linguinePosition = 0; // variable that detect if the copper tape circuits are open or closed because of linguine
 
 void setup() {
   Serial.begin(9600); 
@@ -30,31 +30,35 @@ void loop() {
    // function to open the fridge
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
-    rotateServo(90, 0, false);
-    
+    rotateServo(90, 0);
   }
-  
-  // button that moves Linguine
+  else {
+    Serial.println("low");
+  }
+  // button that moves Linguine's arm
   int movePinRead = digitalRead(buttonPinMove);
   if (movePinRead == HIGH){
-    rotateServo(180, 1, true);
+    rotateServo(90, 1);
   }
-
-  fridgeState = digitalRead(fridgePin);
-  Serial.println(fridgePin);
+  
+  int linguinePosition = digitalRead(linguinePositionPin); // checks if linguine is closing a paper circuit
+  Serial.println(linguinePosition);
+  if (linguinePosition == 1) {
+    Serial.println(linguinePosition);
+    rotateServo(90, 1);
+  }
 }
 
-
-int rotateServo(int degrees, int i, bool rotateOnce){ // function that controls the servos
+int rotateServo(int degrees, int i){ // function that controls the servos
 
   for (pos = 0; pos <= degrees; pos += 1) { // goes from 0 degrees to 180 degrees
+    Serial.println("hi");
     servo[i].write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  if (rotateOnce == false){ // returns the servo back to its original position
-    for (pos = degrees; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      servo[i].write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+  
+  for (pos = degrees; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo[i].write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
