ee6b3c30890056782e81effc71e5e6daf14b72dd
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-14 20:00:03 -0600

    Update servo_stuff.ino
    Allows this code to control 2 servos simulatenously

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -4,20 +4,19 @@ Servo floorservo1;
 Servo floorservo2;
 Servo fridgeservo;
 Servo armservo;
-Servo remyservo;
 
-Servo servo[5] = {floorservo1, floorservo2, fridgeservo, armservo, remyservo};
+Servo servo[] = {floorservo1, floorservo2, fridgeservo, armservo}; // This array stores all the servo objects
 
 const int buttonPin = 2;
 const int fridgePin = 7;
-const int servoPins[] = {9};
+const int servoPins[] = {9, 5, 3, 10};
 int pos = 0;    // variable to store the servo position
 int buttonState = 0; // button status variable
 int fridgeState = 0; // if the fridge is open or not
 
 void setup() {
   Serial.begin(9600); 
-  for(int i = 0; i < 1; i++)
+  for(int i = 0; i < 2; i++) // assigns each servo object a pin on the arduino
   {
     servo[i].attach(servoPins[i]);
   }
@@ -27,6 +26,7 @@ void setup() {
 
 void loop() {
   Serial.println(servoPins[0]);
+  Serial.println(servoPins[1]);
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
     Serial.println("HIGH");
@@ -35,15 +35,18 @@ void loop() {
   else{
     Serial.println("low");
   }
+  
   fridgeState = digitalRead(fridgePin);
   Serial.println(fridgeState);
-  if (fridgeState == 1) {
-    rotateServo(90, 2);
+  if (fridgeState == 0) {
+    Serial.println("open");
+    rotateServo(90, 1);
+    delay(360);
   }
 }
 
 
-int rotateServo(int degrees, int i){
+int rotateServo(int degrees, int i){ // function that controls the servos
 
   for (pos = 0; pos <= degrees; pos += 1) { // goes from 0 degrees to 180 degrees
     Serial.println("hi");
