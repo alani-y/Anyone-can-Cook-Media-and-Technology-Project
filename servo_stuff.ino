0eb9d4c672045c912f3ba68c05ccc515225871ed
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-26 17:43:10 -0600

    Update servo_stuff.ino
    streamlined code and got rid of unnecessary variables

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -1,58 +1,56 @@
 #include <Servo.h>
 
-Servo floorservo;
-Servo floorservo2;
 Servo fridgeservo;
 Servo armservo;
+Servo remyservo;
 
-Servo servo[] = {fridgeservo, armservo}; // This array stores all the servo objects
+Servo servo[] = {fridgeservo, armservo, remyservo}; // This array stores all the servo objects
 
-const int buttonPin = 12;
-const int buttonPinMove = 4;
-const int linguinePositionPin = 8;
-const int servoPins[] = {3, 5};
+const int buttonPin1 = 12;
+const int buttonPin2 = 4;
+const int buttonPin3 = 8;
+const int servoPins[] = {3,5,6};
+
 int pos = 0;    // variable to store the servo position
-int buttonState = 0; // button status variable
-bool rotateOnce = true;
-int linguinePosition = 0; // variable that detect if the copper tape circuits are open or closed because of linguine
+int buttonState1 = 0; // button status variable
+int buttonState2 = 0;
+int buttonState3 = 0;
 
 void setup() {
   Serial.begin(9600); 
-  for(int i = 0; i < 4; i++) // assigns each servo object a pin on the arduino
+  for(int i = 0; i < 3; i++) // assigns each servo object a pin on the arduino
   {
     servo[i].attach(servoPins[i]);
   }
-  pinMode(buttonPin, INPUT);
-  pinMode(buttonPinMove, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
 }
 
 void loop() {
    // function to open the fridge
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
+  buttonState1 = digitalRead(buttonPin1);
+  if (buttonState1 == HIGH) {
     rotateServo(90, 0);
   }
   else {
     Serial.println("low");
   }
   // button that moves Linguine's arm
-  int movePinRead = digitalRead(buttonPinMove);
-  if (movePinRead == HIGH){
+  buttonState2 = digitalRead(buttonPin2);
+  if (buttonState2 == HIGH){
     rotateServo(90, 1);
   }
-  
-  int linguinePosition = digitalRead(linguinePositionPin); // checks if linguine is closing a paper circuit
-  Serial.println(linguinePosition);
-  if (linguinePosition == 1) {
-    Serial.println(linguinePosition);
-    rotateServo(90, 1);
+  // button that moves Remy
+  buttonState3 = digitalRead(buttonPin3);
+  if (buttonState3 == HIGH){
+    rotateServo(90, 2);
   }
 }
 
 int rotateServo(int degrees, int i){ // function that controls the servos
 
   for (pos = 0; pos <= degrees; pos += 1) { // goes from 0 degrees to 180 degrees
-    Serial.println("hi");
+    Serial.println("rotate");
     servo[i].write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
