b7ec6e7f89860bc5d3e4dd709dd46ccbba9fe8cf
AZtheFish <148376550+AZtheFish@users.noreply.github.com>
2023-11-27 09:03:06 -0600

    Update servo_stuff.ino
    added code for the fridge and stove leds

--- servo_stuff.ino
+++ servo_stuff.ino
@@ -9,12 +9,15 @@ Servo servo[] = {fridgeservo, armservo, remyservo}; // This array stores all the
 const int buttonPin1 = 12;
 const int buttonPin2 = 4;
 const int buttonPin3 = 8;
+const int fridgeLedPin = 10;
+const int stoveLedPin = 11;
 const int servoPins[] = {3,5,7};
 
 int pos = 0;    // variable to store the servo position
-int buttonState1 = 0; // button status variable
+int buttonState1 = 0; //   circuit status variable
 int buttonState2 = 0;
 int buttonState3 = 0;
+int stoveButton = 0;
 
 void setup() {
   Serial.begin(9600); 
@@ -24,23 +27,31 @@ void setup() {
   }
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
+  pinMode(fridgeLedPin, OUTPUT);
+  pinMode(stoveLedPin, OUTPUT);
 }
 
 void loop() {
-   // function to open the fridge
+   // circuit to open the fridge
   buttonState1 = digitalRead(buttonPin1);
   if (buttonState1 == HIGH) {
+    digitalWrite(fridgeLedPin, HIGH);
     rotateServo(90, 0);
   }
   else {
     Serial.println("low");
   }
-  // button that moves Linguine's arm
+  // circuit that turns on stove light
+  stoveButton = digitalRead(stoveLedPin);
+  if (stoveButton == HIGH) {
+    digitalWrite(stoveLedPin, OUTPUT);
+  }
+  // circuit that moves Linguine's arm
   buttonState2 = digitalRead(buttonPin2);
   if (buttonState2 == HIGH){
     rotateServo(90, 1);
   }
-  // button that swivels remy around
+  // circuit that moves remy 
   buttonState3 = digitalRead(buttonPin3);
   if (buttonState3 == HIGH){
     rotateServo(90, 2);
@@ -48,7 +59,6 @@ void loop() {
 }
 
 int rotateServo(int degrees, int i){ // function that controls the servos
-
   for (pos = 0; pos <= degrees; pos += 1) { // goes from 0 degrees to 180 degrees
     Serial.println("rotate");
     servo[i].write(pos);              // tell servo to go to position in variable 'pos'
